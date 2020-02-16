08ce59d7666c22648dd60b20ce1212b896f148c3
Vladimir V. Myagdeev <vvmspace@gmail.com>
2020-02-16 21:38:25 +0500

    events list

--- pages/[alias].js
+++ pages/[alias].js
@@ -2,10 +2,18 @@ import { useRouter } from 'next/router';
 import axios from 'axios';
 
 const Post = props => {
+
+    const { venue } = props;
+    const { name, address, events } = venue;
+
     return (
       <div>
-        <h1>{JSON.stringify(props.data)}</h1>
-        <p>This is the blog post content.</p>
+          <h1>{name}</h1>
+        <p>{address}</p>
+        <h2>Концерты в {venue.name}</h2>
+          {events.map(event => (
+              <div><h3>{event.name}</h3></div>
+          ))}
       </div>
     );
   }
@@ -15,15 +23,15 @@ Post.getInitialProps = async function({ query }) {
     const { alias } = query;
 
 
-    const res = axios.get(`http://vvm.space:7007/api/v1/venue/${alias}`);
+    const res = await axios.get(`http://vvm.space:7007/api/v1/venue/${alias}`);
 
 //    const res = await fetch(`http://vvm.space:7007/api/v1/venue/${alias}`);
-    const data = res;
+    const venue = res.data;
   
-    console.log(`Show data fetched. Count: ${data.length}`);
+    console.log(`Show data fetched. Count: ${venue.events.length}`);
   
     return {
-      data
+        venue
     };
   };
 
