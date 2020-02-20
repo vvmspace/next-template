b17edee1fb1052c9d2b47a5312a423b441cdfc3a
Vladimir V. Myagdeev <vvmspace@gmail.com>
2020-02-20 21:14:16 +0500

    -messages + 1 notification

--- components/NavBar.js
+++ components/NavBar.js
@@ -194,13 +194,8 @@ export default function PrimarySearchAppBar(props) {
                     </div>
                     <div className={classes.grow} />
                     <div className={classes.sectionDesktop}>
-                        <IconButton aria-label="show 4 new mails" color="inherit" onClick={showSoon}>
-                            <Badge badgeContent={4} color="secondary">
-                                <MailIcon />
-                            </Badge>
-                        </IconButton>
-                        <IconButton aria-label="show 17 new notifications" color="inherit" onClick={showSoon}>
-                            <Badge badgeContent={17} color="secondary">
+                        <IconButton aria-label="сайт в разработке, скоро будет всё" color="inherit" onClick={showSoon}>
+                            <Badge badgeContent={1} color="secondary">
                                 <NotificationsIcon />
                             </Badge>
                         </IconButton>
