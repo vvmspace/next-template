b70166180eef86d4086250039239310555b1e908
Vladimir V. Myagdeev <vvmspace@gmail.com>
2020-02-21 23:15:44 +0500

    rap & pop

--- pages/pop.js
+++ pages/pop.js
@@ -1,1 +1,23 @@
-export { default } from  './index';
+import GenrePage from "../components/GenrePage/GenrePage";
+import axios from "axios";
+
+const attrs = {
+    title: 'Поп концерты и дискотеки',
+    variations: ['поп', 'pop'],
+    keywords: ['поп', 'попса', 'поп концерты', 'дискотеки'],
+};
+
+const Pop = props => (<GenrePage events={props.events} attrs={attrs}/>);
+
+Pop.getInitialProps = async function() {
+
+    const res = await axios.get(`https://api.concert.moscow/api/v1/event/pop`).catch(e => log(e));
+
+    console.log(res);
+
+    return {
+        events: res && res.data && res.data.events || [],
+    };
+};
+
+export default Pop;
--- pages/rap.js
+++ pages/rap.js
@@ -1,1 +1,23 @@
-export { default } from  './index';
+import GenrePage from "../components/GenrePage/GenrePage";
+import axios from "axios";
+
+const attrs = {
+    title: 'Рэп и хип-хоп концерты',
+    variations: ['рэп', 'rap'],
+    keywords: ['rap', 'рэп', 'hip-hop', 'хип-хоп'],
+};
+
+const Rap = props => (<GenrePage events={props.events} attrs={attrs}/>);
+
+Pop.getInitialProps = async function() {
+
+    const res = await axios.get(`https://api.concert.moscow/api/v1/event/rap`).catch(e => log(e));
+
+    console.log(res);
+
+    return {
+        events: res && res.data && res.data.events || [],
+    };
+};
+
+export default Rap;
