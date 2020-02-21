ba8ded22c3325c9f483e6c95dec033ae7256281d
Vladimir V. Myagdeev <vvmspace@gmail.com>
2020-02-21 16:06:45 +0500

    useSwipeable test

--- components/Layout.js
+++ components/Layout.js
@@ -7,6 +7,7 @@ import Collapse from "@material-ui/core/Collapse";
 import Alert from "@material-ui/core/Collapse";
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
+import { useSwipeable } from 'react-swipeable';
 
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -62,6 +63,8 @@ const Layout = props => {
             menuOpened: false,
         })
     }
+
+    useSwipeable({onSwipedRight: openMenu, onSwipedLeft: closeMenu});
 
     return (
     <div onClick={closeMenu}>
