dfec38e8bf4d80e589a58923064af03230102792
Vladimir V. Myagdeev <vvmspace@gmail.com>
2020-02-22 14:19:22 +0500

    electro.js

---
+++ pages/electro.js
@@ -0,0 +1,23 @@
+import GenrePage from "../components/GenrePage/GenrePage";
+import axios from "axios";
+
+const attrs = {
+    title: 'Электронная музыка',
+    variations: ['рэп', 'rap'],
+    keywords: ['rap', 'рэп', 'hip-hop', 'хип-хоп'],
+};
+
+const Rap = props => (<GenrePage events={props.events} attrs={attrs}/>);
+
+Rap.getInitialProps = async function() {
+
+    const res = await axios.get(`https://api.concert.moscow/api/v1/event/electro`).catch(e => log(e));
+
+    console.log(res);
+
+    return {
+        events: res && res.data && res.data.events || [],
+    };
+};
+
+export default Rap;
