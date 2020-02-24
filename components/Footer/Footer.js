d1c056974d6b88c7129b9c432be393fde9fea941
Vladimir V. Myagdeev <vvmspace@gmail.com>
2020-02-24 20:30:02 +0500

    Footer fixed

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,1 +1,12 @@
-export default () => (<div>Сайт в разработке, не пугайтесь</div>);
+import Container from "@material-ui/core/Container";
+import React from "react";
+export default () => (<Container>
+    <p>Сайт недавно запущен. Разработка продолжается. Отзывы, предложения, пожелания присылайте в Telegram: @vvmspace</p>
+    <p><a href="//www.liveinternet.ru/click" target="_blank">
+        <img
+            src='//counter.yadro.ru/hit?t11.15;r'
+            alt=""
+            title="LiveInternet: показано число просмотров за 24 часа, посетителей за 24 часа и за сегодня"
+            border="0" width="88" height="31" />
+    </a></p>
+</Container>);
