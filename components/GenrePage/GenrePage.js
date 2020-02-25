ab3a025be84d3cd4ea6f0db059db193984d5bdc0
Vladimir V. Myagdeev <vvmspace@gmail.com>
2020-02-25 12:50:24 +0500

    margins

--- components/GenrePage/GenrePage.js
+++ components/GenrePage/GenrePage.js
@@ -2,24 +2,31 @@ import Layout from "../Layout";
 import Typography from "@material-ui/core/Typography";
 import EventCardList from "../EventCardList";
 import Head from 'next/head';
+import { withStyles } from "@material-ui/core/styles";
+
+const withSSR = events.filter(event => event.ssr);
+
+const useStyles = withStyles({
+    subTitle: {
+        marginTop: 20,
+        marginBottom: 10,
+    }
+});
 
 export default props => {
     const {attrs, events} = props;
-
-    const withSSR = events.filter(event => event.ssr);
-
+    const classes = useStyles();
     return (<Layout>
         <Head>
             <title>{attrs.title} в Москве {new Date().getFullYear()}</title>
         </Head>
-        {/*{JSON.stringify(props)}*/}
         <Typography variant={'h3'} component={'h1'}>{attrs.full_title || (<>{attrs.title} в Москве {new Date().getFullYear()}</>)}</Typography>
         <Typography variant={'subtitle1'} component={'p'}>{attrs.subtitle || attrs.description || `Купить билеты на ${attrs.rp || attrs.title.toLowerCase()} без наценки и сервисного сбора`}</Typography>
         <Typography variant={'body1'} component={'p'}>{attrs.body || ''}</Typography>
         {(withSSR.length > 0) && (<>
-            <Typography variant={'h4'} style={{paddingTop: 20}} component={'h2'}>Выбор редакции</Typography>
+            <Typography className={classes.subTitle} variant={'h4'} component={'h2'}>Выбор редакции</Typography>
             <EventCardList sliding showVenue events={withSSR}/>
-            <Typography variant={'h4'} style={{paddingTop: 20}} component={'h2'}>Все события</Typography>
+            <Typography className={classes.subTitle} variant={'h4'} style={{paddingTop: 20}} component={'h2'}>Все события</Typography>
         </>)}
         <EventCardList showVenue events={events}/>
     </Layout>);
