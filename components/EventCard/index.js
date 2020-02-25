c675d924e7d9e92505fc593c3da16e5f3e23ec6a
Vladimir V. Myagdeev <vvmspace@gmail.com>
2020-02-25 13:31:40 +0500

    schema.org

--- components/EventCard/index.js
+++ components/EventCard/index.js
@@ -9,6 +9,7 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import A from '../A';
 import config from "../../config";
+import { EventJsonLd } from "next-seo";
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -54,24 +55,39 @@ const EventCard = props => {
 
     return (
         <Card className={classes.root} style={{width: '100%'}}>
+            <EventJsonLd
+                name={event.name}
+                startDate={event.date}
+                // endDate={event.end_date}
+                location={{
+                    name: event.venue.name,
+                    sameAs: `https://concert.moscow/${event.venue.alias}`,
+                    address: {
+                        streetAddress: event.venue.address,
+                        addressLocality: 'Moscow',
+                        addressRegion: 'Moscow region'
+                    }
+                }}
+                url={`https://concert.moscow/concert/${event.alias || event.uuid}`}
+                description={`Билеты без наценки и сервисного сбора от ${event.min_price} ₽`}
+            />
             <CardActionArea>
                 <A href={link}><CardMedia
                     className={classes.media}
                     image={event.image || '/cm.png'}
                     title={`Концерт ${event.name} в Москве`}
                 /></A>
-                <CardContent itemscope itemtype="http://schema.org/Event">
+                <CardContent>
                     <Typography gutterBottom variant={"h5"} component="h2" className={classes.eventName}>
                         <A
-                            itemprop="name"
                             href={link}
                             title={`Купить билеты на концерт ${event.name} в Москве ${pretty_date} без наценки от ${event.min_price} рублей`}>{
                                 event && event.name && event.name.substr(0, 60).split(' и ')[0]}</A>
                     </Typography>
                     {showVenue
-                     && (<Typography gutterBottom variant="subtitle2" component="p"  itemprop="location" itemscope itemtype="http://schema.org/Place" className={classes.venueName}><A itemprop="name" href={`/${event.venue.alias}`}>{event.venue.name}</A></Typography>)}
+                     && (<Typography gutterBottom variant="subtitle2" component="p"  className={classes.venueName}><A href={`/${event.venue.alias}`}>{event.venue.name}</A></Typography>)}
                     <Typography gutterBottom variant="subtitle1" component="p">
-                        <span itemprop="startDate">{pretty_date}</span>{(event.age > 0) && `, ${event.age}+`}
+                        {pretty_date}{(event.age > 0) && `, ${event.age}+`}
                     </Typography>
                 </CardContent>
             </CardActionArea>
