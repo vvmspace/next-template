3a78ebb94409918e5abc4abec86b203f96a3d2ea
root <root@231357.simplecloud.ru>
2020-02-25 21:06:59 +0300

    EventCardList mobile vision fix

--- components/EventCardList/EventCardList.js
+++ components/EventCardList/EventCardList.js
@@ -25,7 +25,7 @@ const EventCardList = props => {
 
     return (<>
         {/*<Hidden only={props.sliding && 'xs' || []}>*/}
-            <Grid className={classes.theGrid} container spacing={1}>{events
+            <Grid className={props.sliding && classes.theGrid} container spacing={1}>{events
                 .sort((event1, event2) => (new Date(event1.date).getTime() > new Date(event2.date).getTime() && 1 || -1))
                 .map(event => (
                     <Grid item xs={12} sm={6} lg={3} xl={2} md={3}><EventCard showVenue={showVenue} event={event}
