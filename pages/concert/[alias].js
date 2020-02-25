4b673611971109cff79e77e622543e96783bdb79
Vladimir V. Myagdeev <vvmspace@gmail.com>
2020-02-25 16:50:35 +0500

    +offers

--- pages/concert/[alias].js
+++ pages/concert/[alias].js
@@ -34,6 +34,36 @@ const Post = props => {
 
     const goPnm = event => () => (typeof window !== 'undefined') && window.open(event.url + '?promote=concertmoscow') || (() => {});
 
+    const jevent = {
+        "@context": "https://www.schema.org",
+        "@type": "Event",
+        name: event.name,
+        startDate: event.date,
+        location: {
+            "@type": "Place",
+            name: event.venue.name,
+            sameAs: `https://concert.moscow/${event.venue.alias}`,
+            address: {
+                streetAddress: event.venue.address,
+                addressLocality: 'Moscow',
+                addressRegion: 'Moscow region'
+            },
+            offers: {
+                "@type": "Offer",
+                description: "без наценки и сервисного сбора",
+                url: `https://concert.moscow/concert/${event.alias || event.uuid}`,
+                price: event.min_price,
+                priceCurrency : "RUB",
+            }
+        },
+        url: `https://concert.moscow/concert/${event.alias || event.uuid}`,
+        description: `Билеты без наценки и сервисного сбора от ${event.min_price} ₽`
+    }
+
+    if (event.image) {
+        jevent.image = event.image;
+    }
+
     return (
         <Layout>
             <Head>
@@ -43,6 +73,7 @@ const Post = props => {
                 <link rel={'canonical'} href={`https://concert.moscow/concert/${event.alias || event.uuid}`} />
             </Head>
             <Container>
+                {renderHTML(`<script type='application/ld+json'>${JSON.stringify(jevent)}</script>`)}
             <Typography variant="h3" component="h1" gutterBottom>
                 {isConcert && 'Концерт ' || ''}{name} в Москве
             </Typography>
