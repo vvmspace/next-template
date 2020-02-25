0971e812efdfc4473dc1ef09851bf8eadbb53576
Vladimir V. Myagdeev <vvmspace@gmail.com>
2020-02-25 14:54:49 +0500

    liveinternet counter fix

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -4,7 +4,11 @@ export default () => (<Container>
     <p>Сайт недавно запущен. Разработка продолжается. Отзывы, предложения, пожелания присылайте в Telegram: @vvmspace</p>
     <p><a href="//www.liveinternet.ru/click" target="_blank">
         <img
-            src='//counter.yadro.ru/hit?t11.15;r'
+            src={`//counter.yadro.ru/hit?t11.15;r${(typeof window !== 'undefined')
+            && (escape(document.referrer)+((typeof(screen)=='undefined')?'':
+                ';s'+screen.width+'*'+screen.height+'*'+(screen.colorDepth?
+                screen.colorDepth:screen.pixelDepth))+';u'+escape(document.URL)+
+            ';h'+escape(document.title.substring(0,150))+';'+Math.random()) || ''}`}
             alt=""
             title="LiveInternet: показано число просмотров за 24 часа, посетителей за 24 часа и за сегодня"
             border="0" width="88" height="31" />
