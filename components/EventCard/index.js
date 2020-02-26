2112fe4f94b10cbc42346166f06276b569409a2b
Vladimir V. Myagdeev <vvmspace@gmail.com>
2020-02-26 21:43:17 +0500

    ${new Date(event.date).getFullYear()}

--- components/EventCard/index.js
+++ components/EventCard/index.js
@@ -114,7 +114,7 @@ const EventCard = props => {
                 <A key={props.key} className={classes.aWrap} href={link}><CardMedia
                     className={classes.media}
                     image={event.image || '/cm.png'}
-                    title={`Концерт ${event.name} в Москве ${new Date().getFullYear()}`}
+                    title={`Концерт ${event.name} в Москве ${new Date(event.date).getFullYear()}`}
                     key={props.key}
                 />
                 <CardContent key={props.key}>
