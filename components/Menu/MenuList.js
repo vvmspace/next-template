9cd6cd0d61f4c74abe2ff8b28b1782996f425210
Vladimir V. Myagdeev <vvmspace@gmail.com>
2020-02-26 18:37:49 +0500

    Блок

--- components/Menu/MenuList.js
+++ components/Menu/MenuList.js
@@ -1,9 +1,13 @@
 import 'react';
 import List from '@material-ui/core/List';
 import MenuItem from "./MenuItem";
+import {ListItemText, ListItem} from "@material-ui/core";
 
-export default props => (<List>{
-    props.items && props.items.map(({text, href, icon, iconColor, title}) => (
+export default props => (<List>{<>
+    <ListItem>
+        <ListItemText>Блок</ListItemText>
+    </ListItem>
+    {props.items && props.items.map(({text, href, icon, iconColor, title}) => (
         <MenuItem text={text} href={href.includes('http') && href || `https://concert.moscow${href}`} icon={icon} iconColor={iconColor} title={title || text}/>
-    ))
-}</List>);
+    ))}
+</>}</List>);
