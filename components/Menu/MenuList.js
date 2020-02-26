400e97f456bd576c5ab56d411b63e2fe3bafe7fa
Vladimir V. Myagdeev <vvmspace@gmail.com>
2020-02-26 18:32:16 +0500

    force https://concert.moscow

--- components/Menu/MenuList.js
+++ components/Menu/MenuList.js
@@ -4,6 +4,6 @@ import MenuItem from "./MenuItem";
 
 export default props => (<List>{
     props.items && props.items.map(({text, href, icon, iconColor, title}) => (
-        <MenuItem text={text} href={href} icon={icon} iconColor={iconColor} title={title || text}/>
+        <MenuItem text={text} href={href.includes('http') && href || `https://concert.moscow${href}`} icon={icon} iconColor={iconColor} title={title || text}/>
     ))
 }</List>);
