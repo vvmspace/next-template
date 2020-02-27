b7262ece9c0022c4718dfe77b9ab43516ee97980
Vladimir V. Myagdeev <vvmspace@gmail.com>
2020-02-27 20:46:37 +0500

    +1 commit, github check

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -3,6 +3,7 @@ import React from "react";
 export default () => (<div style={{backgroundColor: '#448b00',
     paddingTop: 10,
     boxShadow: '4px 2px 0px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)'
+
 }}>
     <Container>
     {(typeof window !== 'undefined') && (<>
