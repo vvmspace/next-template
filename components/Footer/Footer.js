e7c99487302aafb379b0201f50ec7796744bc828
Vladimir V. Myagdeev <vvmspace@gmail.com>
2020-02-27 23:40:32 +0500

    -color

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,13 +1,6 @@
 import Container from "@material-ui/core/Container";
 import React from "react";
-export default () => (<div style={{backgroundColor: '#448b00',
-    paddingTop: 10,
-    boxShadow: '4px 2px 0px -1px rgba(0,0,0,0.2), 4px 4px 0px 0px rgba(0,0,0,0.14), 10px 1px 0px 0px rgba(0,0,0,0.12)',
-    color: '#fff',
-    paddingBottom: 5
-
-}}>
-    <Container>
+export default () => (<Container>
     {(typeof window !== 'undefined') && (<>
         <p>Powered by Vladimir V. Myagdeev (<a href={'mailto:concert.moscow@vvm.space'}>concert.moscow@vvm.space</a>), 2020</p>
     <p>Сайт запущен 23 февраля 2020 г. Отзывы, предложения, пожелания присылайте в Telegram: @vvmspace</p>
@@ -23,4 +16,4 @@ export default () => (<div style={{backgroundColor: '#448b00',
             border="0" width="88" height="31" />
     </a></p>
     </>)}
-</Container></div>);
+</Container>);
