7fbf8007bb882509998145740719a06e88914eea
Vladimir V. Myagdeev <vvmspace@gmail.com>
2020-02-29 09:01:01 +0500

    electro description

--- pages/electro.js
+++ pages/electro.js
@@ -4,9 +4,13 @@ import axios from "axios";
 const attrs = {
     title: 'Электронная музыка',
     variations: ['Electro', 'elec'],
-    keywords: ['rap', 'рэп', 'hip-hop', 'хип-хоп'],
+    keywords: ['electro', 'drum', 'house', 'electro house', 'trance'],
     description: 'Купить билеты на концерты и фестивали электронной музыки в Москве',
     rp: 'электронной музыки',
+    metatags: {
+        description: 'Electro, Electro House, Trance, Progressive House - тусовки и концерты в Москве',
+        keywords: 'Electro, Electro House, Trance, Progressive House'
+    }
 };
 
 const Genre = props => (<GenrePage events={props.events} attrs={attrs}/>);
