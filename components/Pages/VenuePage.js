903d458836fa5e2dce5b47f63dca1838f98fd2d8
Vladimir V. Myagdeev <vvmspace@gmail.com>
2020-03-02 17:00:28 +0500

    VenuePage.js

---
+++ components/Pages/VenuePage.js
@@ -0,0 +1,47 @@
+import {titleCase} from "title-case";
+import Layout from "../Layout";
+import Head from "next/head";
+import {Divider, Typography} from "@material-ui/core";
+import EventCardList from "../EventCardList";
+import React from "react";
+
+const VenuePage = props => {
+
+    const {venue} = props;
+    const {name: _name, address, events, alias, uuid} = venue;
+    const name = titleCase(_name);
+    const sorted = {
+        events: events
+            .filter(event => new Date(event.date) > new Date())
+            .sort(
+                (event1, event2) =>
+                    (new Date(event1.date).getTime() > new Date(event2.date).getTime() && 1 || -1))
+    };
+
+    return (
+        <Layout>
+            <Head>
+                <title>Концерты в {name} | concert.moscow - билеты на концерты в Москве без наценки и сервисного
+                    сбора</title>
+                <meta httpEquiv={'description'}
+                      content={`Афиша концертов в ${name}. Официальные билеты без наценки и сервисного сбора.`}/>
+                <link rel={'canonical'} href={`https://concert.moscow/${alias || uuid}`}/>
+            </Head>
+            <Typography variant="h2" component="h1" gutterBottom>
+                {name}
+            </Typography>
+            <Typography variant="subtitle1" component="p" gutterBottom>
+                {address}
+            </Typography>
+            <Typography variant="h4" component="p" gutterBottom>
+                Концерты в {name}
+            </Typography>
+            <Typography variant="subtitle1" component="p" gutterBottom>
+                Благодаря нашим партнёрам вы можете <b>купить билеты на концерты в {name} без наценки и сервисного
+                сбора</b>
+            </Typography>
+            <Divider/>
+            <EventCardList events={sorted.events}/>
+        </Layout>
+    );
+}
--- pages/[alias].js
+++ pages/[alias].js
@@ -7,44 +7,47 @@ const { api_url } = config;
 import Head from "next/head";
 import { titleCase } from "title-case";
 import React from "react";
+import VenuePage from '../components/Pages/VenuePage';
 
-const Post = props => {
+const Post = props => (<VenuePage venue={props.venue}/>);
 
-    const { venue } = props;
-    const { name:_name, address, events, alias, uuid } = venue;
-    const name = titleCase(_name);
-    const sorted = {
-        events: events
-            .filter(event => new Date(event.date) > new Date())
-            .sort(
-            (event1, event2) =>
-            (new Date(event1.date).getTime() > new Date(event2.date).getTime() && 1 || -1))
-    };
-
-    return (
-      <Layout>
-          <Head>
-              <title>Концерты в {name} | concert.moscow - билеты на концерты в Москве без наценки и сервисного сбора</title>
-              <meta httpEquiv={'description'} content={`Афиша концертов в ${name}. Официальные билеты без наценки и сервисного сбора.`} />
-              <link rel={'canonical'} href={`https://concert.moscow/${alias || uuid}`} />
-          </Head>
-          <Typography variant="h2" component="h1" gutterBottom>
-              {name}
-          </Typography>
-          <Typography variant="subtitle1" component="p" gutterBottom>
-              {address}
-          </Typography>
-          <Typography variant="h4" component="p" gutterBottom>
-              Концерты в {name}
-          </Typography>
-          <Typography variant="subtitle1" component="p" gutterBottom>
-              Благодаря нашим партнёрам вы можете <b>купить билеты на концерты в {name} без наценки и сервисного сбора</b>
-          </Typography>
-          <Divider/>
-          <EventCardList events={sorted.events} />
-      </Layout>
-    );
-  };
+// const Post = props => {
+//
+//     const { venue } = props;
+//     const { name:_name, address, events, alias, uuid } = venue;
+//     const name = titleCase(_name);
+//     const sorted = {
+//         events: events
+//             .filter(event => new Date(event.date) > new Date())
+//             .sort(
+//             (event1, event2) =>
+//             (new Date(event1.date).getTime() > new Date(event2.date).getTime() && 1 || -1))
+//     };
+//
+//     return (
+//       <Layout>
+//           <Head>
+//               <title>Концерты в {name} | concert.moscow - билеты на концерты в Москве без наценки и сервисного сбора</title>
+//               <meta httpEquiv={'description'} content={`Афиша концертов в ${name}. Официальные билеты без наценки и сервисного сбора.`} />
+//               <link rel={'canonical'} href={`https://concert.moscow/${alias || uuid}`} />
+//           </Head>
+//           <Typography variant="h2" component="h1" gutterBottom>
+//               {name}
+//           </Typography>
+//           <Typography variant="subtitle1" component="p" gutterBottom>
+//               {address}
+//           </Typography>
+//           <Typography variant="h4" component="p" gutterBottom>
+//               Концерты в {name}
+//           </Typography>
+//           <Typography variant="subtitle1" component="p" gutterBottom>
+//               Благодаря нашим партнёрам вы можете <b>купить билеты на концерты в {name} без наценки и сервисного сбора</b>
+//           </Typography>
+//           <Divider/>
+//           <EventCardList events={sorted.events} />
+//       </Layout>
+//     );
+//   };
 
 Post.getInitialProps = async function({ query }) {
 
