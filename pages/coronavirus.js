ad9a4c508ed02b77bcc19c9f437ccc22c80bb010
Vladimir V. Myagdeev <vvmspace@gmail.com>
2020-03-31 00:00:13 +0500

    +text

--- pages/coronavirus.js
+++ pages/coronavirus.js
@@ -5,13 +5,16 @@ import Typography from "@material-ui/core/Typography";
 export default props => {
     return (<Layout>
         <Head>
-            <title>Коронавирус в Москве 2020: отмена меропиятий по причине вспышки Covid-19</title>
+            <title>Коронавирус в Москве 2020: отмена меропиятий и карантин по причине вспышки Covid-19</title>
             {/*{_meta && _meta.description && (<meta name={'description'} content={_meta.description}/>)}*/}
             {/*{_meta && _meta.keywords && (<meta name={'keywords'} content={_meta.keywords}/>)}*/}
         </Head>
         <Typography variant={'h3'} component={'h1'}>Коронавирус в Москве 2020</Typography>
         <Typography variant={'body1'} component={'p'}>Ситуация развивается динамически. Сайт переведён в автономный режим. Мы не можем отслеживать весь новостной поток и проверять внешие источники. Информация может быть не актуальной.</Typography>
         <Typography variant={'h4'} component={'h2'}>Отмена массовых мероприятий в Москве</Typography>
+        <Typography variant={'body1'} component={'p'}>В начале вспышки пандемии <strong>Covid-19</strong> в Москве были запрещены массовые мероприятия численностью более 5000 человек. Фронтмен Rammstein Тилль Линдеманн провёл 2 концерта в Москве и был госпитализирован с атипичной пневмонией.</Typography>
+        <Typography variant={'h4'} component={'h2'}>Карантин и комендантский час в Москве</Typography>
+        <Typography variant={'body1'} component={'p'}>Начиная с 30 апреля 2020 года в Москве введёт жесткий карантин и комендантский час. Сидите дома.</Typography>
 
     </Layout>);
 }
